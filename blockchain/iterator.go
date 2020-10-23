35d7f0562267d5317ce77d3757579fb0a31d2729
eyuelberga <eyuel.berga@yahoo.com>
2020-10-23 21:45:57 +0300

    :pencil: description on functions

--- blockchain/iterator.go
+++ blockchain/iterator.go
@@ -3,18 +3,18 @@ package blockchain
 import (
 	"emailchain/presistence"
 )
-
+// Iterator interface
 type Iterator interface {
 	Next() *Block
 	HasNext() bool
 }
-
+// Blockchain Iterator structure
 type BlockchainIterator struct {
 	CurrentHash string
 	Index       int64
 	db          *presistence.Database
 }
-
+// Create new iterator
 func NewBlockchainIterator(bc Blockchain) *BlockchainIterator {
 	return &BlockchainIterator{
 		CurrentHash: string(bc.Db.Tip()),
@@ -22,13 +22,14 @@ func NewBlockchainIterator(bc Blockchain) *BlockchainIterator {
 		db:          bc.Db,
 	}
 }
-
+// Go to next block in the blockchain database
 func (bi *BlockchainIterator) Next() *Block {
 	block := DeserializeBlock(bi.db.GetBlock(bi.CurrentHash))
 	bi.CurrentHash = block.PreviousHash
 	bi.Index--
 	return block
 }
+// Check if there is a next block in the blockchain database
 func (bi *BlockchainIterator) HasNext() bool {
 	if bi.Index < 0 {
 		return false
